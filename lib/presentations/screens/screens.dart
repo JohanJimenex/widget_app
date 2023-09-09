4c183632aaf3e6c7b0ce1e02756216c0e8f78bf0
johanjimenex <johan.jimenez.23@gmail.com>
2023-09-09 01:09:40 -0400

    crea demas pantallas

--- lib/presentations/screens/screens.dart
+++ lib/presentations/screens/screens.dart
@@ -1,6 +1,12 @@
 //Esto es un archivo barril, que exporta codigo para importarlo mas bonito desde otros archivos,
 //Solo es para reducir las lineas de codigo de importaciones
 
+export 'package:widget_app/presentations/screens/animateds/animated_screen.dart';
+export 'package:widget_app/presentations/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_app/presentations/screens/buttons/buttons_screen.dart';
 export 'package:widget_app/presentations/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentations/screens/home/home_screen.dart';
+export 'package:widget_app/presentations/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widget_app/presentations/screens/progress/progress_screen.dart';
+export 'package:widget_app/presentations/screens/snackbar/snackbar_screen.dart';
+export 'package:widget_app/presentations/screens/ui_control/ui_control_screen.dart';
